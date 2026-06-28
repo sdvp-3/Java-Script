14480453c66fe195d34f54a4035626e3f7bc462d
k-3200 <kuziyevsaidjon06@gmail.com>
2026-06-29 04:36:58 +0900

    All in one finished

--- FINISHED/todo-list-project/todo-list.js
+++ FINISHED/todo-list-project/todo-list.js
@@ -1,43 +1,41 @@
-let toDoList = []
+let toDoList = [];
 
-const saved = localStorage.getItem('todos');
-if(saved) {
+const saved = localStorage.getItem("todos");
+if (saved) {
   toDoList = JSON.parse(saved);
   addToDo(toDoList);
 }
 
+function addToDo(arr) {
+  let toDoListHtml = "";
 
-function addToDo(arr){
-  let toDoListHtml="";
-
-  for(let i=0; i<arr.length; i++){
-toDoListHtml += `<div class="divs"
+  for (let i = 0; i < arr.length; i++) {
+    toDoListHtml += `<div class="divs"
  
 ">  <span class="t1-span">${arr[i].name}</span>
     <span class="t2-span">${arr[i].date}</span> <button class="remove" onclick="removeToDo(${i})"
->Remove</button> </div>`
+>Remove</button> </div>`;
   }
-   document.querySelector('.js-show').innerHTML = toDoListHtml;
+  document.querySelector(".js-show").innerHTML = toDoListHtml;
 }
 
-document.querySelector('.js-btn').addEventListener('click', ()=>{
- const name = document.querySelector('.js-input').value
-  const date = document.querySelector('.js-date').value
+document.querySelector(".js-btn").addEventListener("click", () => {
+  const name = document.querySelector(".js-input").value;
+  const date = document.querySelector(".js-date").value;
 
-  if (name==='') return;
+  if (name === "") return;
 
-  toDoList.push({name: name,date: date})
+  toDoList.push({ name: name, date: date });
 
-  localStorage.setItem('todos', JSON.stringify(toDoList));
-  addToDo(toDoList)
+  localStorage.setItem("todos", JSON.stringify(toDoList));
+  addToDo(toDoList);
 
-
-   document.querySelector('.js-input').value = '';
-  document.querySelector('.js-date').value = '';
+  document.querySelector(".js-input").value = "";
+  document.querySelector(".js-date").value = "";
 });
 
 function removeToDo(index) {
   toDoList.splice(index, 1);
-   localStorage.setItem('todos', JSON.stringify(toDoList));
+  localStorage.setItem("todos", JSON.stringify(toDoList));
   addToDo(toDoList);
 }
