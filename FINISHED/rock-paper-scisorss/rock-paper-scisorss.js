e363b5879020ab815662bf89e37e8643c48f1f00
k-3200 <kuziyevsaidjon06@gmail.com>
2026-06-28 11:50:47 +0900

    Finished mini projects

---
+++ FINISHED/rock-paper-scisorss/rock-paper-scisorss.js
@@ -0,0 +1,72 @@
+let score = JSON.parse(localStorage.getItem("js-score")) || {
+  wins: 0,
+  losses: 0,
+  ties: 0,
+};
+
+function playGame(playerMove) {
+  const computerMove = pickComputerMove();
+
+  let result = "";
+
+  if (playerMove === "Rock") {
+    if (computerMove === "Rock") {
+      result = "Tie";
+    } else if (computerMove === "Paper") {
+      result = "Lost";
+    } else if (computerMove === "Scissors") {
+      result = "Win";
+    }
+  } else if (playerMove === "Paper") {
+    if (computerMove === "Rock") {
+      result = "Win";
+    } else if (computerMove === "Paper") {
+      result = "Tie";
+    } else if (computerMove === "Scissors") {
+      result = "Lost";
+    }
+  } else if (playerMove === "Scissors") {
+    if (computerMove === "Rock") {
+      result = "Lost";
+    } else if (computerMove === "Paper") {
+      result = "Win";
+    } else if (computerMove === "Scissors") {
+      result = "Tie";
+    }
+  }
+  if (result === "Win") {
+    score.wins += 1;
+  } else if (result === "Lost") {
+    score.losses += 1;
+  } else if (result === "Tie") {
+    score.ties += 1;
+  }
+
+  localStorage.setItem("js-score", JSON.stringify(score));
+  scores();
+  document.querySelector(".js-result").innerHTML = result;
+
+  document.querySelector(".js-move").innerHTML =
+    `👤:${playerMove}-${computerMove}:🖥️`;
+}
+
+function scores() {
+  document.querySelector(".js-score").innerHTML =
+    `🏆:${score.wins},❌:${score.losses},🤝:${score.ties}`;
+}
+
+function pickComputerMove() {
+  const randomNumber = Math.random();
+  console.log(`Random number:${randomNumber}`)
+  let computerMove = "";
+
+  if (randomNumber > 0 && randomNumber < 0.33) {
+    computerMove = "Rock";
+  } else if (randomNumber >0.33 && randomNumber < 0.66) {
+    computerMove = "Paper";
+  } else if (randomNumber >0.66 && randomNumber < 0.99) {
+    computerMove = "Scissors";
+  }
+
+  return computerMove;
+}
---
+++ FINISHED/todo-list-project/todo-list.js
@@ -0,0 +1,43 @@
+let toDoList = []
+
+const saved = localStorage.getItem('todos');
+if(saved) {
+  toDoList = JSON.parse(saved);
+  addToDo(toDoList);
+}
+
+
+function addToDo(arr){
+  let toDoListHtml="";
+
+  for(let i=0; i<arr.length; i++){
+toDoListHtml += `<div class="divs"
+ 
+">  <span class="t1-span">${arr[i].name}</span>
+    <span class="t2-span">${arr[i].date}</span> <button class="remove" onclick="removeToDo(${i})"
+>Remove</button> </div>`
+  }
+   document.querySelector('.js-show').innerHTML = toDoListHtml;
+}
+
+document.querySelector('.js-btn').addEventListener('click', ()=>{
+ const name = document.querySelector('.js-input').value
+  const date = document.querySelector('.js-date').value
+
+  if (name==='') return;
+
+  toDoList.push({name: name,date: date})
+
+  localStorage.setItem('todos', JSON.stringify(toDoList));
+  addToDo(toDoList)
+
+
+   document.querySelector('.js-input').value = '';
+  document.querySelector('.js-date').value = '';
+});
+
+function removeToDo(index) {
+  toDoList.splice(index, 1);
+   localStorage.setItem('todos', JSON.stringify(toDoList));
+  addToDo(toDoList);
+}
